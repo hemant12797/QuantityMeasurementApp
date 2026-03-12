25559906edd81cf7f1e471cd2164e4f229a9d7ed
hemant12797 <hemantthakur8439@gmail.com>
2026-03-12 12:32:20 +0530

    [Hemant Kumar]Updated UC15

---
+++ QuantityMeasurementApp/QuantityMeasurement.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using QuantityMeasurement.Business.Services;
+using QuantityMeasurement.Repository;
+using QuantityMeasurement.ConsoleApp.Menu;
+
+IQuantityService service = new QuantityService(new QuantityRepository());
+
+IMenu menu = new Menu(service);
+
+menu.Start();
