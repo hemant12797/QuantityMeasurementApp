1b00bee6ad78a4b4fca024e637e38e27323d8489
hemant12797 <hemantthakur8439@gmail.com>
2026-03-30 10:37:19 +0530

    UC17 DONE

---
+++ QuantityMeasurement/ModelLayer/Models/User.cs
@@ -0,0 +1,11 @@
+namespace ModelLayer.Models
+{
+    public class User
+    {
+        public int UserId {get; set;}
+        public string FirstName {get; set;} = string.Empty;
+        public string LastName {get; set;} = string.Empty;
+        public string Email {get; set;} = string.Empty;
+        public string HashedPassword {get; set;} = string.Empty;
+    }
+}
